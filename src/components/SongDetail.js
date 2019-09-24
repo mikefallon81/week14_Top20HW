33ad6bbbcbc6d89b808400c307b888501f5f3920
mikefallon <mikefallon81@gmail.com>
2019-09-24 21:35:38 +0100

    css positioning added

--- src/components/SongDetail.js
+++ src/components/SongDetail.js
@@ -4,12 +4,14 @@ import './songDetail.css';
 const SongDetail = (props) => {
 
   return(
+    <container>
     <div className="songDetails">
     <h2>Song Title: {props.title}</h2>
     <h3>Artist: {props.artist}</h3>
     <h3>Chart Position: {props.chartPosition}</h3>
     <button><a href={props.preview}>Preview</a></button>
     </div>
+    </container>
   )
 }
 
