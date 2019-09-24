314298b425b96b4aa2a25f708d7d93e2ab68f573
mikefallon <mikefallon81@gmail.com>
2019-09-24 21:18:46 +0100

    styling added

--- src/components/SongDetail.js
+++ src/components/SongDetail.js
@@ -1,14 +1,14 @@
 import React from 'react';
+import './songDetail.css';
 
 const SongDetail = (props) => {
 
   return(
-    <div>
+    <div className="songDetails">
     <h2>Song Title: {props.title}</h2>
     <h3>Artist: {props.artist}</h3>
     <h3>Chart Position: {props.chartPosition}</h3>
-  
-
+    <button><a href={props.preview}>Preview</a></button>
     </div>
   )
 }
